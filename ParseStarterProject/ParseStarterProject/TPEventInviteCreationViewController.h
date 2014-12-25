99e5c17f512b98e9fe19fc37319670bc8ea03fdb
Tyler Pfaff <tpfaff@blastmotion.com>
2014-12-25 02:04:52 -0800

    added event creation view controller

---
+++ ParseStarterProject/ParseStarterProject/TPEventInviteCreationViewController.h
@@ -0,0 +1,13 @@
+//
+//  TPEventInviteCreationViewController.h
+//  ParseStarterProject
+//
+//  Created by Tyler Pfaff on 12/25/14.
+//
+//
+
+#ifndef ParseStarterProject_TPEventInviteCreationViewController_h
+#define ParseStarterProject_TPEventInviteCreationViewController_h
+
+
+#endif
---
+++ ParseStarterProject/ParseStarterProject/TPEventInviteCreationViewController.m
@@ -0,0 +1,9 @@
+//
+//  TPEventInviteCreationViewController.m
+//  ParseStarterProject
+//
+//  Created by Tyler Pfaff on 12/25/14.
+//
+//
+
+#import <Foundation/Foundation.h>
