0808a62fb1460ac84a2776432098a991757dcf57
Tyler Pfaff <tylerpfaff@gmail.com>
2015-01-13 01:05:34 -0800

    Added friends search

---
+++ ParseStarterProject/ParseStarterProject/TPAddFriendsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TPAddFriendsTableViewController.h
+//  Down
+//
+//  Created by Tyler Pfaff on 1/12/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPAddFriendsTableViewController : UITableViewController
+
+@end
