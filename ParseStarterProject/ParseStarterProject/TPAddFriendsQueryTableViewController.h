98dcf047977751dacef441b4426e93c522f024a9
Tyler Pfaff <tylerpfaff@gmail.com>
2015-01-13 22:05:12 -0800

    Added query table view class

---
+++ ParseStarterProject/ParseStarterProject/TPAddFriendsQueryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TPAddFriendsQueryTableViewController.h
+//  Down
+//
+//  Created by Tyler Pfaff on 1/13/15.
+//
+//
+
+#import <ParseUI/ParseUI.h>
+
+@interface TPAddFriendsQueryTableViewController : PFQueryTableViewController
+
+@end
---
+++ ParseStarterProject/ParseStarterProject/TPAddFriendsQueryTableViewController.m
@@ -0,0 +1,37 @@
+//
+//  TPAddFriendsQueryTableViewController.m
+//  Down
+//
+//  Created by Tyler Pfaff on 1/13/15.
+//
+//
+
+#import "TPAddFriendsQueryTableViewController.h"
+
+@interface TPAddFriendsQueryTableViewController ()
+
+@end
+
+@implementation TPAddFriendsQueryTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
